4b0caa1e464b3ed82edfb0c8f069197c443f1f5a
IanDarwin <>
2011-09-29 01:34:32 +0000

    TabbyText, the purr-fect way to send SMS Text Messages from your Android Table.

---
+++ src/com/darwinsys/tabbytext/Main.java
@@ -0,0 +1,38 @@
+package com.darwinsys.tabbytext;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inf = getMenuInflater();
+		inf.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+		case R.id.about:
+			startActivity(new Intent(this, AboutActivity.class));
+			return true;
+		}
+		return false;
+	}
+}
