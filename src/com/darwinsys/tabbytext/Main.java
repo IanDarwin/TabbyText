64253acd50798987fce54094518e1144fe09ec3d
IanDarwin <>
2011-10-01 19:23:15 +0000

    Tabbify

--- src/com/darwinsys/tabbytext/Main.java
+++ src/com/darwinsys/tabbytext/Main.java
@@ -19,14 +19,15 @@ import android.widget.Toast;
 
 public class Main extends Activity {
 	private static final int REQ_GET_CONTACT = 1;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        Button b = (Button) findViewById(R.id.contactChoose);
-        b.setOnClickListener(new View.OnClickListener() {			
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Button b = (Button) findViewById(R.id.contactChoose);
+		b.setOnClickListener(new View.OnClickListener() {			
 			@Override
 			public void onClick(View arg0) {
 				Uri uri = Uri.parse("content://contacts/people");
@@ -34,53 +35,53 @@ public class Main extends Activity {
 				startActivityForResult(intent, REQ_GET_CONTACT);
 			}
 		});
-    }
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	if (requestCode == REQ_GET_CONTACT) {
-    		switch(resultCode) {
-    		case Activity.RESULT_OK:
-    			// The Contacts API is about the most complex to use.
-    			// First we have to retrieve the Contact, since we only get its URI from the Intent
-    			Uri resultUri = data.getData(); // e.g., content://contacts/people/123
-    			Cursor cont = getContentResolver().query(resultUri, null, null, null, null);
-    			if (!cont.moveToNext()) {	// expect 001 row(s)
-    				Toast.makeText(this, "Cursor contains no data", Toast.LENGTH_LONG).show(); 
-        			return;
-    			}
-    			int columnIndexForId = cont.getColumnIndex(ContactsContract.Contacts._ID);
-    			String contactId = cont.getString(columnIndexForId);
-    			int columnIndexForHasPhone = cont.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
-    			boolean hasAnyPhone = Boolean.parseBoolean(cont.getString(columnIndexForHasPhone));
-    			if (!hasAnyPhone) {
-    				Toast.makeText(this, "Selected contact has no phone numbers ", Toast.LENGTH_LONG).show(); 
-    				return;
-    			}
-    			// Now we have to do another query to actually get the numbers!
-    			Cursor numbers = getContentResolver().query(
-    					ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
-    					null, 
-    					ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, // "selection", 
-    					null, null);
-    			while (numbers.moveToNext()) {
-    				String aNumber = numbers.getString(numbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-    				System.out.println(aNumber);
-    				return;
-    			}
-    			Toast.makeText(this, "Selected contact has phone numbers but we didn't find them!", Toast.LENGTH_LONG).show(); 
-    			break;
-    		case Activity.RESULT_CANCELED:
-    			// nothing to do here
-    			break;
-    		default:
-    			Toast.makeText(this, "Unexpected resultCode: " + resultCode, Toast.LENGTH_LONG).show(); 
-    			break;
-    		}
-    	}
-    	super.onActivityResult(requestCode, resultCode, data);
-    }
-    
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQ_GET_CONTACT) {
+			switch(resultCode) {
+			case Activity.RESULT_OK:
+				// The Contacts API is about the most complex to use.
+				// First we have to retrieve the Contact, since we only get its URI from the Intent
+				Uri resultUri = data.getData(); // e.g., content://contacts/people/123
+				Cursor cont = getContentResolver().query(resultUri, null, null, null, null);
+				if (!cont.moveToNext()) {	// expect 001 row(s)
+					Toast.makeText(this, "Cursor contains no data", Toast.LENGTH_LONG).show(); 
+					return;
+				}
+				int columnIndexForId = cont.getColumnIndex(ContactsContract.Contacts._ID);
+				String contactId = cont.getString(columnIndexForId);
+				int columnIndexForHasPhone = cont.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+				boolean hasAnyPhone = Boolean.parseBoolean(cont.getString(columnIndexForHasPhone));
+				if (!hasAnyPhone) {
+					Toast.makeText(this, "Selected contact has no phone numbers ", Toast.LENGTH_LONG).show(); 
+					return;
+				}
+				// Now we have to do another query to actually get the numbers!
+				Cursor numbers = getContentResolver().query(
+						ContactsContract.CommonDataKinds.Phone.CONTENT_URI, 
+						null, 
+						ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, // "selection", 
+						null, null);
+				while (numbers.moveToNext()) {
+					String aNumber = numbers.getString(numbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+					System.out.println(aNumber);
+					return;
+				}
+				Toast.makeText(this, "Selected contact has phone numbers but we didn't find them!", Toast.LENGTH_LONG).show(); 
+				break;
+			case Activity.RESULT_CANCELED:
+				// nothing to do here
+				break;
+			default:
+				Toast.makeText(this, "Unexpected resultCode: " + resultCode, Toast.LENGTH_LONG).show(); 
+				break;
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
