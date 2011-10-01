156066bc4a4faa14f184000d4ffc41a973956bde
IanDarwin <>
2011-10-01 16:25:57 +0000

    Start adding code to pick the Contact and retreive it.

--- src/com/darwinsys/tabbytext/Main.java
+++ src/com/darwinsys/tabbytext/Main.java
@@ -1,18 +1,62 @@
 package com.darwinsys.tabbytext;
 
+import java.util.Set;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends Activity {
+	private static final int REQ_GET_CONTACT = 1;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button b = (Button) findViewById(R.id.contactChoose);
+        b.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				Uri uri = Uri.parse("content://contacts/people");
+				Intent intent = new Intent(Intent.ACTION_PICK, uri);
+				startActivityForResult(intent, REQ_GET_CONTACT);
+			}
+		});
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if (requestCode == REQ_GET_CONTACT) {
+    		switch(resultCode) {
+    		case Activity.RESULT_OK:
+    			// retrieve the Contact
+    			Bundle ret = data.getExtras();
+    			if (ret != null) {
+    				Set<String> keys = ret.keySet();
+    				Log.d("ret", "Extras: Key size is "+ keys.size());
+    				for (String key : keys) {
+    					Log.d("ret", ret.get(key).toString());
+    				}
+    			}
+    			break;
+    		case Activity.RESULT_CANCELED:
+    			// nothing to do here
+    			break;
+    		default:
+    			Toast.makeText(this, "Unexpected resultCode: " + resultCode, Toast.LENGTH_LONG).show(); 
+    			break;
+    		}
+    	}
+    	super.onActivityResult(requestCode, resultCode, data);
     }
     
 	@Override
